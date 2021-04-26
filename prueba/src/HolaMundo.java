13868de4d06853ced23b5391b8f00a6bbe690789
Profesor <Profesor@DESKTOP-0FE7I1O>
2021-04-26 19:45:29 +0100

    commit inicial

---
+++ prueba/src/HolaMundo.java
@@ -0,0 +1,15 @@
+
+import java.io.IOException;
+
+
+public class HolaMundo {
+	
+	public static void main(String[] args) throws IOException {
+      
+        System.out.println("---------------------------------------------");
+        System.out.println(" HOLA MUNDO");
+        System.out.println("---------------------------------------------");
+        
+        
+	}
+}
