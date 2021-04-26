3031102adca4ec26b0b7af61b1e7a3e4d8d84ab9
Profesor <Profesor@DESKTOP-0FE7I1O>
2021-04-26 19:49:23 +0100

    commit

--- prueba/src/HolaMundo.java
+++ prueba/src/HolaMundo.java
@@ -7,7 +7,7 @@ public class HolaMundo {
 	public static void main(String[] args) throws IOException {
       
         System.out.println("---------------------------------------------");
-        System.out.println(" HOLA MUNDO");
+        System.out.println(" HOLA MUNDO PRUEBA 2");
         System.out.println("---------------------------------------------");
         
         
